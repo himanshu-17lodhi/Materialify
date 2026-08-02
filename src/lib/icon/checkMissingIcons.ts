5d3df496582b904bdb4168e59c6f062816199b0c
himanshu <lodhihimanshu17@gmail.com>
2026-08-03 00:18:18 +0530

    refactor: split icon module

---
+++ src/lib/icon/checkMissingIcons.ts
@@ -0,0 +1,171 @@
+import config from '@/config';
+import { Icon } from '@/engine';
+import {
+  getNormalizedName,
+  getSvgFromLoadedIcon,
+  nextIdentifier,
+} from '@/engine/util';
+import { LUCIDE_ICON_PACK_NAME } from '@/engine/lucide';
+import emoji from '@/emoji';
+import { logger } from '@/lib/logger-service';
+import IconizePlugin, { FolderIconObject } from '@/main';
+import dom from '@/utils/dom';
+
+import { Notice } from 'obsidian';
+
+export default async function checkMissingIcons(
+  plugin: IconizePlugin,
+  data: [string, string | FolderIconObject][],
+): Promise<void> {
+  const missingIcons: Set<Icon> = new Set();
+  const allIcons: Map<string, boolean> = new Map();
+
+  const getMissingIcon = async (
+    iconNameWithPrefix: string,
+  ): Promise<Icon | null> => {
+    const iconNextIdentifier = nextIdentifier(iconNameWithPrefix);
+    const iconName = iconNameWithPrefix.substring(iconNextIdentifier);
+    const iconPrefix = iconNameWithPrefix.substring(0, iconNextIdentifier);
+
+    const iconPack = plugin
+      .getIconPackManager()
+      .getIconPackByPrefix(iconPrefix);
+
+    if (!iconPack) {
+      return null;
+    }
+
+    if (
+      iconPack.getName() === LUCIDE_ICON_PACK_NAME &&
+      !plugin.doesUseCustomLucideIconPack()
+    ) {
+      return null;
+    }
+
+    const icon = iconPack.getIcon(iconName);
+
+    if (!icon) {
+      logger.error(
+        `Icon file with name ${iconNameWithPrefix} could not be found`,
+      );
+      return null;
+    }
+
+    const doesIconFileExists = await plugin.app.vault.adapter.exists(
+      `${plugin.getIconPackManager().getPath()}/${iconPack.getName()}/${iconName}.svg`,
+    );
+
+    if (!doesIconFileExists) {
+      const possibleIcon = getSvgFromLoadedIcon(plugin, iconPrefix, iconName);
+
+      if (!possibleIcon) {
+        logger.error(
+          `Icon SVG with name ${iconNameWithPrefix} could not be found`,
+        );
+        return null;
+      }
+
+      await plugin.getIconPackManager().extractIcon(icon, possibleIcon);
+
+      return icon;
+    }
+
+    return null;
+  };
+
+  for (const rule of plugin.getSettings().rules) {
+    if (!emoji.isEmoji(rule.icon)) {
+      allIcons.set(rule.icon, true);
+
+      const icon = await getMissingIcon(rule.icon);
+
+      if (icon) {
+        missingIcons.add(icon);
+      }
+    }
+  }
+
+  for (const [, value] of data) {
+    let iconNameWithPrefix = value as string;
+
+    if (typeof value === 'object') {
+      iconNameWithPrefix = value.iconName;
+    }
+
+    if (iconNameWithPrefix && !emoji.isEmoji(iconNameWithPrefix)) {
+      allIcons.set(iconNameWithPrefix, true);
+
+      const icon = await getMissingIcon(iconNameWithPrefix);
+
+      if (icon) {
+        missingIcons.add(icon);
+      }
+    }
+  }
+
+  if (missingIcons.size !== 0) {
+    new Notice(
+      `[${config.PLUGIN_NAME}] Background Check: found missing icons. Adding missing icons...`,
+      10000,
+    );
+  }
+
+  for (const icon of missingIcons) {
+    const normalizedName = getNormalizedName(icon.prefix + icon.name);
+
+    const nodesWithIcon = document.querySelectorAll(
+      `[${config.attributes.icon}="${normalizedName}"]`,
+    );
+
+    nodesWithIcon.forEach((node: HTMLElement) => {
+      dom.setIconForNode(plugin, normalizedName, node);
+    });
+  }
+
+  if (missingIcons.size !== 0) {
+    new Notice(
+      `[${config.PLUGIN_NAME}] Background Check: added missing icons`,
+      10000,
+    );
+  }
+
+  for (const iconPack of plugin.getIconPackManager().getIconPacks()) {
+    const doesIconPackExist = await plugin.app.vault.adapter.exists(
+      `${plugin.getIconPackManager().getPath()}/${iconPack.getName()}`,
+    );
+
+    if (!doesIconPackExist) {
+      continue;
+    }
+
+    const iconFiles = await plugin.app.vault.adapter.list(
+      `${plugin.getIconPackManager().getPath()}/${iconPack.getName()}`,
+    );
+
+    for (const iconFilePath of iconFiles.files) {
+      const iconNameWithExtension = iconFilePath.split('/').pop();
+
+      const iconName = iconNameWithExtension?.substring(
+        0,
+        iconNameWithExtension.length - 4,
+      );
+
+      const iconNameWithPrefix = iconPack.getPrefix() + iconName;
+      const doesIconExist = allIcons.get(iconNameWithPrefix);
+
+      if (!doesIconExist) {
+        const path = `${plugin.getIconPackManager().getPath()}/${iconPack.getName()}/${iconName}.svg`;
+
+        const doesPathExist = await plugin.app.vault.adapter.exists(path);
+
+        if (doesPathExist) {
+          logger.info(
+            `Removing icon with path '${path}' because it is not used anymore`,
+          );
+
+          await plugin.app.vault.adapter.remove(path);
+        }
+      }
+    }
+  }
+}
---
+++ src/lib/icon/index.ts
@@ -0,0 +1,15 @@
+import addAll from './addAll';
+import checkMissingIcons from './checkMissingIcons';
+import getAllWithPath from './getAllWithPath';
+import getByPath from './getByPath';
+import getIconByName from './getIconByName';
+import getIconByPath from './getIconByPath';
+
+export default {
+  addAll,
+  checkMissingIcons,
+  getAllWithPath,
+  getByPath,
+  getIconByName,
+  getIconByPath,
+};
